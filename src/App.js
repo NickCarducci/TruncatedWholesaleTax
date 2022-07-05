667aeb4294bbbf4b46b7a00725c06d47bf76492c
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 12:53:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -179,294 +179,6 @@ export default class App extends React.Component {
           }}
         />
         <h2 style={{ float: "right", width: "200px" }}>
-          pay your fair share = pay cops less but relatively more
-        </h2>
-        <h1>
-          <a
-            href="https://www.quora.com/Does-weed-make-you-less-angry-like-me"
-            style={{ textDecoration: "none" }}
-          >
-            90's negatively correlated
-          </a>
-        </h1>
-        I won the case said homicides and weed everyone <br />
-        should they are about to eat my ass
-        <br />
-        Sewage is a matter of national security
-        <br />
-        Excess
-        <br />
-        Destroy the{space}
-        <a href="https://sewageisamatterofdomesticviolence.quora.com">
-          toilet-caste
-        </a>
-        <br />
-        FBI/GSA could stop 25% offending by 2005-2012 recidivists alone
-        (hopefully homicide gets more than 10 years with that rate…
-        <h4>
-          All or nothing! 30% 18-25 weed, 10% 26+{space}
-          <a href="https://nickcarducciforsenate.quora.com/Is-marijuana-use-prevalent-in-accidents-causing-fatalities-1">
-            <span role="img" aria-label="mapel-leaf">
-              🍁
-            </span>
-          </a>
-        </h4>
-        sexual health 15m instead of monoamine however you reuptake it licensure
-        removeing liability and causing violence (Menendez animal rights rat
-        abuse but not a horse on grill?). Is dissolved to libido in "simple
-        assault" as sexual harassment
-        <br />
-        *quora mods
-        <br />
-        "is it the job of the state's insurance to revoke your libido if the
-        <br />
-        gender that you are attracted to keeps rejecting you?" "O.K."
-        <br />
-        <Petition />
-        "sex addiction" ok, muddy up that topic (stigmatize-sex)
-        <br />
-        surveying quora mods, one by one, use all the psychosis terms
-        <br />
-        Creates "survey bias" topic, literally.*
-        <br />
-        "flag-bearers for obesity" is thought ableist? is free speech in
-        <br />
-        protected in the First Amendment in regards to 501(c)(3)
-        <br />
-        no answers: 'automatic question-edit review..'
-        <br />
-        people want me to invent things for my competitors' unemployment
-        benefits?
-        <br />
-        when I displace them bartenders should pay for proximity-housing and
-        {space}[
-        <a href="https://www.quora.com/unanswered/Does-banking-the-unbanked-include-bartenders-so-we-dont-have-to-hold-spend-cash-quarterly-in-a-1099-for-tax-exempt-camps-tuition-and-medical">
-          potentially-another
-        </a>
-        ]{space}
-        business-relevant tuition.
-        <Benefits
-          lastWidth={Math.min(600, this.state.lastWidth)}
-          style={{
-            paddingBottom: "15px",
-            backgroundColor: "rgb(190,150,180)"
-          }}
-        />
-        <br />
-        Covid is significantly excess by a sample of 2019-2020?
-        <br />
-        the data doesn't go that far back, 5x1 cohortxyear is 1950-
-        <br />
-        <h3>
-          <a href="https://census.quora.com/How-old-is-too-old-to-be-a-boomer-1">
-            CDC took credit for this one
-          </a>
-        </h3>
-        homicide 90's so we should really be getting everyone high,
-        bureau.quora.com and
-        <br />
-        federalreserve.quora.com - justice statistics, econ anal & labor stat
-        <br />
-        insurance surrendered bid - EULA banking play a different game (third
-        party benefiticary)
-        <br />
-        max royalties islamic banking (disburse upon sales to partnets
-        <br />
-        with vertical expense line), reverse amortization controlled shmita
-        <br />
-        truncated wholesale tax is like, already done*
-        <br />
-        marginal value added grows faster than lifetime inflation by insurance
-        by tech
-        <br />
-        <Cable
-          style={{
-            backgroundColor: "white",
-            width: "100%",
-            height: "auto"
-          }}
-          onError={handleScollImgError}
-          img={true}import React from "react";
-//import TwitterTweetEmbed from "./TwitterTweetEmbed";
-import { UAParser } from "ua-parser-js";
-import Benefits from "./benefits";
-import Cable from "./Dropwire";
-import Turnout from "./njturnout";
-import Petition from "./Petition";
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    var parser = new UAParser();
-    const name = parser.getBrowser().name;
-    console.log(name);
-    this.state = {
-      trigger: false,
-      browser: name,
-      serviceCancelingImages: name.includes("Safari")
-    };
-    for (let i = 0; i < 220; i++) {
-      this["scrollImg" + i] = React.createRef();
-    }
-  }
-  componentDidMount = () => {
-    window.addEventListener("resize", this.refresh);
-    window.addEventListener("scroll", this.handleScroll);
-    this.refresh(true);
-    this.handleScroll();
-  };
-  componentWillUnmount = () => {
-    clearTimeout(this.check);
-    clearTimeout(this.scrollTimeout);
-    clearTimeout(this.resizeTimer);
-    window.removeEventListener("resize", this.refresh);
-    window.removeEventListener("scroll", this.handleScroll);
-  };
-  handleScroll = (e) => {
-    if (!this.state.offScroll) {
-      const scrollTop = window.scrollY;
-      this.setState(
-        {
-          scrolling: true,
-          scrollTop
-        },
-        () => {
-          clearTimeout(this.scrollTimeout);
-          this.scrollTimeout = setTimeout(() => {
-            this.setState({
-              scrolling: false
-            });
-          }, 900);
-        }
-      );
-    }
-  };
-  refresh = (first) => {
-    const width =
-      (this.state.ios ? window.screen.availWidth : window.innerWidth) - 20;
-    if (first || Math.abs(this.state.lastWidth - width) > 0) {
-      clearTimeout(this.resizeTimer);
-      this.resizeTimer = setTimeout(() => {
-        this.setState({
-          lastWidth: width,
-          width,
-          availableHeight: this.state.ios
-            ? window.screen.availHeight - 20
-            : window.innerHeight
-        });
-      }, 600);
-    }
-  };
-  componentDidUpdate = (prevProps) => {
-    if (this.props.pathname !== prevProps.pathname) {
-      clearTimeout(this.check);
-      const check = () => {
-        if (this.props.pathname !== "/") {
-          this.setState({ trigger: true });
-        }
-        if (this.props.pathname === "/edu") {
-          // window.scroll(0, this.edu.current.offsetTop);
-        }
-      };
-      check();
-      this.check = setTimeout(check, 4000);
-    }
-  };
-  render() {
-    const handleScollImgError = (e) => {
-      if (e.message) {
-        console.log(e.message);
-        this.setState({ serviceCancelingImages: true });
-      }
-    };
-    let arrayOfnumbers = 0;
-    const scrollnum = () => {
-      arrayOfnumbers = arrayOfnumbers + 1; //arrayOfnumbers[arrayOfnumbers.length - 1] + 1;
-      //arrayOfnumbers.push(num);
-      //console.log(arrayOfnumbers)
-      return arrayOfnumbers;
-    };
-    const space = " ";
-    return (
-      <div
-        style={{
-          overflow: "hidden",
-          //margin: "5px",
-          fontFamily: "arial, sans serif",
-          wordBreak: "break-word",
-          textAlign: "left",
-          width: "100%",
-          maxWidth: "600px",
-          position: "relative"
-        }}
-      >
-        Jester.com.co - minimal viable duress & corpus linguistics for division
-        of labor, utility{space}
-        <b>
-          less work and productive for less economic productivity expense per
-          hour
-        </b>
-        .
-        <br />
-        <a
-          href="https://truncatedwholesaletax.quora.com"
-          style={{
-            float: "right",
-            width: `calc(${Math.min(600, this.state.width)}px - 70%)`
-          }}
-        >
-          <Cable
-            style={{
-              backgroundColor: "white",
-              width: "100%",
-              height: "auto"
-            }}
-            onError={handleScollImgError}
-            img={true}
-            src={
-              this.state.noyout
-                ? ""
-                : "https://www.dropbox.com/s/6movjouiifpm79h/waluigi.png?raw=1"
-            }
-            float={null}
-            title="https://truncatedwholesaletax.quora.com"
-            scrolling={this.state.scrolling}
-            fwd={this["scrollImg" + scrollnum()]}
-            scrollTopAndHeight={this.state.scrollTop + window.innerHeight}
-            scrollTop={this.state.scrollTop}
-          />
-        </a>
-        <h2>
-          <a href="https://www.quora.com/unanswered/Can-you-pay-yourself-through-a-self-employment-account-as-a-bartender-to-not-pay-income-tax-before-cost-of-living-expenses">
-            Can you pay yourself through a self-employment account as a
-            bartender to not pay income tax before cost-of-living expenses?
-          </a>
-        </h2>
-        <h2>
-          <a href="https://www.quora.com/unanswered/What-do-you-think-about-unemployment-benefits-for-market-crashes-when-financial-advisors-disclaim-losses-to-their-clients">
-            What do you think about unemployment benefits for market crashes
-            when financial advisors disclaim losses to their clients?
-          </a>
-        </h2>
-        "Billions to survive," for ongoing{space}
-        <a href="https://implausibleuseleases.quora.com">
-          implausible use leases
-        </a>
-        {space}beyond 5 stores and condos, "a proper{space}
-        <a href="https://courttechnology.quora.com/Is-affordable-housing-a-public-good-1">
-          job
-        </a>
-        ."
-        <br />
-        <br />
-        <Turnout
-          lastWidth={Math.min(600, this.state.lastWidth)}
-          style={{
-            paddingBottom: "15px",
-            backgroundColor: "rgb(190,150,180)"
-          }}
-        />
-        <h2 style={{ float: "right", width: "200px" }}>
           pay your fair{space}
           <a href="https://www.quora.com/Are-state-rights-not-unconstitutional-because-it-is-not-uniform-nor-tranquil-for-all-classes-as-in-the-division-of-labor">
             share
@@ -505,8 +217,11 @@ export default class App extends React.Component {
         </h4>
         sexual health 15m instead of monoamine however you reuptake it licensure
         removeing liability and causing violence (Menendez animal rights rat
-        abuse but not a horse on grill?). Is dissolved to libido in "simple
-        assault" as sexual harassment
+        abuse but not a horse on grill?). Is dissolved to libido in "
+        <a href="https://beingstable.quora.com/Is-it-always-wrong-to-discriminate-on-the-basis-of-schizophrenia-1">
+          simple assault
+        </a>
+        " as sexual harassment
         <br />
         *quora mods
         <br />
@@ -653,7 +368,15 @@ export default class App extends React.Component {
         <a href="https://leisureutilization.quora.com/Is-socialism-overrated-1">
           socialism is overrated
         </a>
-        {space}(my rich family is in{space}
+        {space}(my{space}
+        <a href="https://landlordliquidity.quora.com/What-does-a-market-crash-mean-in-real-estate-1">
+          rich
+        </a>
+        {space}
+        <a href="https://www.quora.com/Why-is-the-infrastructure-in-the-USA-so-rundown/answer/Nick-Carducci">
+          family
+        </a>
+        {space}is in{space}
         <a href="https://froth.app">infrastructure</a>)
         <h3>I lost my friends to wall st</h3>
         john catsimitidis says drop dead socialists, I say{space}
