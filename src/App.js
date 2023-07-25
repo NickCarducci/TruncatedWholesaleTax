7975638300cef315a1410067c36e820f6f6aaa12
Nick Carducci <nmcarducci@gmail.com>
2023-07-25 08:55:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends React.Component {
           Nicholas Carducci is running for Senate. In order to run for office
           under the banner-slogan “Bankruptcy is Fraud”,{space}
           <i>
-            Nick Carducci needs 762 more signatures! He would prefer to
+            Nick Carducci needs 756 more signatures! He would prefer to
             {space}
             <b>discharge</b>
             {space}student debt.
