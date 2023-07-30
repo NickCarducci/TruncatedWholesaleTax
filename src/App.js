f03ebd6d967f158e9c6b1a091c4aa691274d7ac1
Nick Carducci <nmcarducci@gmail.com>
2023-07-30 09:32:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -84,8 +84,8 @@ class App extends React.Component {
           Nicholas Carducci is running for Senate against Menendez's animal
           rights for horses but not lab rats. He also wants very easy
           immigration policies but doesn't mind a border. He doesn't understand
-          how real GDP is non-zero and thinks deflation is better than just
-          disinflation. In order to run for office under the banner-slogan
+          how real GDP growth is non-zero and thinks deflation is better than
+          just disinflation. In order to run for office under the banner-slogan
           “Bankruptcy is Fraud”,{space}
           <i>
             Nick Carducci needs 750 more signatures! He would prefer to
@@ -274,5 +274,4 @@ class App extends React.Component {
   }
 }
 export default App;
-
 
