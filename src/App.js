6837bde4244c50300c97d73372fc91833ec6b499
Nick Carducci <nmcarducci@gmail.com>
2023-08-24 09:43:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -103,17 +103,17 @@ class App extends React.Component {
             compound interest and collateral repossession instead of industry
             assessment, as in payday loans, surrenders the purchases of others;
             and contracts generally are bonk in liberty but for shares and
-            voting: never surrender that which is not yours in contract law
-            means shares and voting, not compound interest, collateral
-            repossession, nor implausible use leases. He generally leans right
-            on government spending and left on monopolies, wanting very easy
-            immigration policies, yet doesn't mind a border. He also believes
-            the Hippocratic Oath, namely for sutures and natal care, can replace
-            most of medicaid, less transplants and social security liabilities
-            can be paid by wealthy retirees instead of an income tax. The
-            Federal Reserve should be the only lender; he wants the central bank
-            to pay for any deficit, while tax rates rise to remit the year
-            before.
+            voting: meanwhile, to never surrender that which is not yours in
+            contract law means shares and voting, not compound interest,
+            collateral repossession, nor implausible use leases. He generally
+            leans right on government spending and left on monopolies, wanting
+            very easy immigration policies, yet doesn't mind a border. He also
+            believes the Hippocratic Oath, namely for sutures and natal care,
+            can replace most of medicaid, less transplants, and social security
+            liabilities can be paid by wealthy retirees instead of an income
+            tax. The Federal Reserve should be the only lender; he wants the
+            central bank to pay for any deficit, while tax rates rise to remit
+            the year before.
             {
               space +
                 space /*He thinks the income and inheritance tax can be replaced by a wealth
